eef8885acdab45db9d6892227fd1014c25c84a3d
Kevin Yobeth <leokeviny@gmail.com>
2020-04-02 14:33:05 +0700

    Clean plate

--- main.py
+++ main.py
@@ -31,49 +31,57 @@ data = data.drop(data[data.sentiment == 'neutral'].index)
 data = data.drop(data[data.sentiment == 'worry'].index)
 
 # Making all letters lowercase
-data['content'] = data['content'].apply(lambda x: " ".join(x.lower() for x in x.split()))
+data['content'] = data['content'].apply(
+    lambda x: " ".join(x.lower() for x in x.split()))
 
 # Removing Punctuation, Symbols
-data['content'] = data['content'].str.replace('[^\w\s]',' ')
+data['content'] = data['content'].str.replace('[^\w\s]', ' ')
 
 # Removing Stop Words using NLTK
 stop = stopwords.words('english')
-data['content'] = data['content'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
+data['content'] = data['content'].apply(
+    lambda x: " ".join(x for x in x.split() if x not in stop))
 
-#Lemmatisation
-data['content'] = data['content'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
-#Correcting Letter Repetitions
+# Lemmatisation
+data['content'] = data['content'].apply(lambda x: " ".join(
+    [Word(word).lemmatize() for word in x.split()]))
+# Correcting Letter Repetitions
+
 
 def de_repeat(text):
     pattern = re.compile(r"(.)\1{2,}")
     return pattern.sub(r"\1\1", text)
 
-data['content'] = data['content'].apply(lambda x: " ".join(de_repeat(x) for x in x.split()))
+
+data['content'] = data['content'].apply(
+    lambda x: " ".join(de_repeat(x) for x in x.split()))
 
 # Code to find the top 10,000 rarest words appearing in the data
 freq = pd.Series(' '.join(data['content']).split()).value_counts()[-10000:]
 
 # Removing all those rarely appearing words from the data
 freq = list(freq.index)
-data['content'] = data['content'].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
+data['content'] = data['content'].apply(
+    lambda x: " ".join(x for x in x.split() if x not in freq))
 
-#Encoding output labels 'sadness' as '1' & 'happiness' as '0'
+# Encoding output labels 'sadness' as '1' & 'happiness' as '0'
 lbl_enc = preprocessing.LabelEncoder()
 y = lbl_enc.fit_transform(data.sentiment.values)
 
 # Splitting into training and testing data in 90:10 ratio
-X_train, X_val, y_train, y_val = train_test_split(data.content.values, y, stratify=y, random_state=42, test_size=0.1, shuffle=True)
+X_train, X_val, y_train, y_val = train_test_split(
+    data.content.values, y, stratify=y, random_state=42, test_size=0.1, shuffle=True)
 
 # Extracting TF-IDF parameters
-tfidf = TfidfVectorizer(max_features=1000, analyzer='word',ngram_range=(1,3))
+tfidf = TfidfVectorizer(max_features=1000, analyzer='word', ngram_range=(1, 3))
 X_train_tfidf = tfidf.fit_transform(X_train)
 X_val_tfidf = tfidf.fit_transform(X_val)
 
 # Extracting Count Vectors Parameters
 count_vect = CountVectorizer(analyzer='word')
 count_vect.fit(data['content'])
-X_train_count =  count_vect.transform(X_train)
-X_val_count =  count_vect.transform(X_val)
+X_train_count = count_vect.transform(X_train)
+X_val_count = count_vect.transform(X_val)
 
 # Model 1: Multinomial Naive Bayes Classifier
 nb = MultinomialNB()
@@ -103,7 +111,7 @@ y_pred = rf.predict(X_val_tfidf)
 print('random forest tfidf accuracy %s' % accuracy_score(y_pred, y_val))
 # random forest tfidf accuracy 0.5385356454720617
 
-## Building models using count vectors feature
+# Building models using count vectors feature
 # Model 1: Multinomial Naive Bayes Classifier
 nb = MultinomialNB()
 nb.fit(X_train_count, y_train)
@@ -129,35 +137,34 @@ print('log reg count vectors accuracy %s' % accuracy_score(y_pred, y_val))
 rf = RandomForestClassifier(n_estimators=500)
 rf.fit(X_train_count, y_train)
 y_pred = rf.predict(X_val_count)
-print('random forest with count vectors accuracy %s' % accuracy_score(y_pred, y_val))
+print('random forest with count vectors accuracy %s' %
+      accuracy_score(y_pred, y_val))
 # random forest with count vectors accuracy 0.7524084778420038
 
-#Below are 8 random statements. The first 4 depict happiness. The last 4 depict sadness
+# Below are 8 random statements. The first 4 depict happiness. The last 4 depict sadness
 
-# tweets = pd.DataFrame(['I am very happy today! The atmosphere looks cheerful',
-# 'Things are looking great. It was such a good day',
-# 'Success is right around the corner. Lets celebrate this victory',
-# 'Everything is more beautiful when you experience them with a smile!',
-# 'Now this is my worst, okay? But I am gonna get better.',
-# 'I am tired, boss. Tired of being on the road, lonely as a sparrow in the rain. I am tired of all the pain I feel',
-# 'This is quite depressing. I am filled with sorrow',
-# 'His death broke my heart. It was a sad day'])
-
-tweets = pd.DataFrame(['I am very sad', 'I am very happy'])
+tweets = pd.DataFrame(['I am very happy today! The atmosphere looks cheerful',
+                       'Things are looking great. It was such a good day',
+                       'Success is right around the corner. Lets celebrate this victory',
+                       'Everything is more beautiful when you experience them with a smile!',
+                       'Now this is my worst, okay? But I am gonna get better.',
+                       'I am tired, boss. Tired of being on the road, lonely as a sparrow in the rain. I am tired of all the pain I feel',
+                       'This is quite depressing. I am filled with sorrow',
+                       'His death broke my heart. It was a sad day'])
 
 # Doing some preprocessing on these tweets as done before
-tweets[0] = tweets[0].str.replace('[^\w\s]',' ')
-from nltk.corpus import stopwords
+tweets[0] = tweets[0].str.replace('[^\w\s]', ' ')
 stop = stopwords.words('english')
-tweets[0] = tweets[0].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-from textblob import Word
-tweets[0] = tweets[0].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+tweets[0] = tweets[0].apply(lambda x: " ".join(
+    x for x in x.split() if x not in stop))
+tweets[0] = tweets[0].apply(lambda x: " ".join(
+    [Word(word).lemmatize() for word in x.split()]))
 
 # Extracting Count Vectors feature from our tweets
 tweet_count = count_vect.transform(tweets[0])
 
-#Predicting the emotion of the tweet using our already trained linear SVM
+# Predicting the emotion of the tweet using our already trained linear SVM
 tweet_pred = lsvm.predict(tweet_count)
 print(tweet_pred)
-## result
-## [0 0 0 0 1 1 1 1]
+# result
+# [0 0 0 0 1 1 1 1]
